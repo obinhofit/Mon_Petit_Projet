9d4cc9861ec11c9d8e6de84ebffdd0b05fbd2e54
La Taniaire <aubin.dadie2026@campus-eni.fr>
2026-04-19 21:36:03 +0200

    MàJ3

--- pages/CheckoutPage.py
+++ pages/CheckoutPage.py
@@ -3,16 +3,18 @@ from selenium.webdriver.common.by import By
 
 
 
-class CardPage():
-    # constructeur
+class CheckoutPage():
+
+    # --/  Constructeur
     def __init__(self, driver: WebDriver):
         self.driver = driver
-        self.but_checkout = '//a[@class="btn btn-lg btn-primary"]' # simple quote pour les string
+        self.guest_checkout = '//Label[contains(text(), "Guest Checkout")]' # simple quote pour les string
         #self.link_first_item = '//img[@title="Canon EOS 5D"]'
 
-    def Click_But_Checkout(self):
-        c_but_checkout = self.driver.find_element(By.XPATH, self.but_checkout)
-        c_but_checkout.click()
+    # --/ Méthode
+    def Select_Guest_Checkout(self):
+        c_guest_checkout = self.driver.find_element(By.XPATH, self.guest_checkout)
+        c_guest_checkout.click()
 
     #def Click_But_Checkout(self):
             #c_but_checkout = self.driver.find_element(By.XPATH, self.but_checkout)
